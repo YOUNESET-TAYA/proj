4c5f49cee0d242d448674481f1f6fe3a56a195f4
YOUNESET-TAYA <younes.et-taya.51@edu.uiz.ac.ma>
2026-05-12 16:51:50 +0200

    Add files via upload

--- src/module-info.java
+++ src/module-info.java
@@ -4,5 +4,5 @@
 /**
  * 
  */
-module TP20 {
+module TPJAVA7 {
 }
---
+++ src/pqrtie1/Shopexpress.java
@@ -0,0 +1,26 @@
+package pqrtie1;
+
+public class shopexpress {
+
+	public double effectuerpaiment(double montant,double solde) throws Exception {
+		if(montant<=0) {
+			throw new Exception("ereur le mantant doit etre supérieur a zero");
+		}
+		if(montant>solde) {
+			throw new Exception("solde insufaisent");
+		}
+		return solde-montant;
+
+	}
+
+	public double liremontant(String string) {
+		
+		return 0;
+	}
+	
+
+}
+ring string) {
+		
+		return 0;
+	}
